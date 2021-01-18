64bba5bf894f2ee0672b40e230cb8eed6414f91b
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-01-18 17:16:07 +0900

    fix: ページネーションデザイン変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-30.times do |i|
+100.times do |i|
 	User.create(email: "user_#{i}@example.com", password: "password")
 end
