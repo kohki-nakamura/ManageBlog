fc4088bda591ec5830f80b1f3b0c00a927d9f211
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-01-20 03:29:04 +0900

    change: サインイン後、サインアウト後のリダイレクト先を指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+	def after_sign_in_path_for(resource)
+		if current_user.is_admin?
+			admin_root_path
+		else
+			root_path
+		end
+	end
+
+	def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
 end
