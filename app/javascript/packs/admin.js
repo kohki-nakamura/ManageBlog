666633b2463a23b26bef3ee8693fce20cd9ed2b8
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-01-19 18:25:47 +0900

    change: admin.jsで読み込んでいたjsをcdnで読み込むよう変更

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -10,18 +10,7 @@ import "channels"
 
 // Volt
 import "./admin/volt.js"
-// === 以下を読み込むと何故かActionTextが表示されなくなる ===
-// === なくても問題なさそうなので一旦コメントアウト ===
-// import "./admin/vendor/popper.min.js"
-// import "./admin/vendor/on-screen.umd.min.js"
-// import "./admin/vendor/nouislider.min.js"
-// import "./admin/vendor/jarallax.min.js"
-// import "./admin/vendor/smooth-scroll.polyfills.min.js"
-// import "./admin/vendor/countUp.umd.js"
-// import "./admin/vendor/chartist.min.js"
-// import "./admin/vendor/chartist-plugin-tooltip.min.js"
-// import "./admin/vendor/datepicker.min.js"
-// import "./admin/vendor/simplebar.min.js"
+import "./admin/vendor/on-screen.umd.min.js"
 
 Rails.start()
 Turbolinks.start()
