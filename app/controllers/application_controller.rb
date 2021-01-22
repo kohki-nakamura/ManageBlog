5a33d6b1579fa858512f2ddec9106dbf628268d4
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-01-22 23:17:59 +0900

    add: ログイン画面にデザイン適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	layout :layout_by_resource
+
 	def after_sign_in_path_for(resource)
 		if current_user.is_admin?
 			admin_root_path
@@ -9,5 +11,15 @@ class ApplicationController < ActionController::Base
 
 	def after_sign_out_path_for(resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
+  end
+
+	protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise/layouts/application"
+    else
+      "application"
+    end
   end
 end
